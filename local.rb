71bff311bd7cdc324d5c4f296b3c3054bb1c5df8
xixitalk <xixinxing@gmail.com>
2013-12-03 16:07:38 +0800

    fix bug

--- local.rb
+++ local.rb
@@ -30,15 +30,19 @@ cfg_file = File.open('config.json')
 config =  JSON.parse(cfg_file.read)
 cfg_file.close
 
-$direct_cfg_file = File.open('direct.json')
-$directList_def =  JSON.parse($direct_cfg_file.read)
-$directList = $directList_def.collect { |x| x.to_sym.object_id}
-$direct_cfg_file.close
+cfg_file = File.open('direct.json')
+$directList =  JSON.parse(cfg_file.read)
+$directList.collect! { |x| x.to_sym.object_id}
+cfg_file.close
 
-$block_cfg_file = File.open('block.json')
-$blockList_def =  JSON.parse($block_cfg_file.read)
-$blockList = $blockList_def.collect { |x| x.to_sym.object_id}
-$block_cfg_file.close
+cfg_file = File.open('block.json')
+$blockList =  JSON.parse(cfg_file.read)
+$blockList.collect! { |x| x.to_sym.object_id}
+cfg_file.close
+
+cfg_file = File.open('other.json')
+$otherDict =  JSON.parse(cfg_file.read)
+cfg_file.close
 
 key = config['password']
 
@@ -46,32 +50,32 @@ $server = config['server']
 $remote_port = config['server_port'].to_i
 $port = config['local_port'].to_i
 
-puts "init config finished"
-
 $encrypt_table, $decrypt_table = get_table(key)
 
 def inet_ntoa(n)
     n.unpack("C*").join "."
 end
 
-def addSiteFile(fd,list_def,list,site)
-	list_def.push(site)
-	list.push(site.to_sym.object_id)
-	JSON.dump(list_def,fd)
+def add2OtherDictFile(host,port)
+  if host == nil or port == nil then return end
+  $otherDict[host] = port
+  cfg_file = File.open('other.json','w')
+  JSON.dump($otherDict,cfg_file)
+  cfg_file.close
 end
 
-def isDirectConnectFunc(site)
-    status = if $blockList.include?(site.to_sym.object_id) then false
-    elsif $directList.include?(site.to_sym.object_id) then true
-    else true
-    end
+def isProxyConnectFunc(site)
+  status = if $blockList.include?(site.to_sym.object_id) then false
+  elsif $directList.include?(site.to_sym.object_id) then true
+  else false
+  end
 end
 
 def isClassifyFunc(site)
-    status = if $directList.include?(site.to_sym.object_id) then true
-    elsif $blockList.include?(site.to_sym.object_id) then true
-    else false
-    end
+  status = if $directList.include?(site.to_sym.object_id) then true
+  elsif $blockList.include?(site.to_sym.object_id) then true
+  else false
+  end
 end
 
 module LocalServer
@@ -83,14 +87,14 @@ module LocalServer
 
     def post_init
       #p "connecting #{@server.remote_addr} via #{@server.server_using}"
-      if not @server.isDirectConnect
+      if @server.isProxyConnect
         addr_to_send = @server.addr_to_send.clone
-      	encrypt $encrypt_table, addr_to_send
+        encrypt $encrypt_table, addr_to_send
         send_data addr_to_send
       end
 
       for piece in @server.cached_pieces
-      	if not @server.isDirectConnect
+        if @server.isProxyConnect
           encrypt $encrypt_table, piece
         end
         send_data piece
@@ -101,28 +105,11 @@ module LocalServer
     end
 
     def receive_data data
-      if not @server.isDirectConnect
+      if @server.isProxyConnect
         encrypt $decrypt_table, data
       end
-      @server.dataCount += data.size
       @server.send_data data
     end
-    
-      def connection_completed
-      	@server.isCompleted = true
-  	    #puts "#{__LINE__} #{@server.remote_addr} completed"
-  	    if not isClassifyFunc(@server.remote_addr)
-  	    	#puts "#{@server.remote_addr} is not classify"
-  	    	if @server.isDirectConnect and @server.dataCount > 2000
-  	    		cfg_file = File.open('direct.json','w')
-  	    		addSiteFile(cfg_file,$directList_def,$directList,@server.remote_addr)
-  	    	else
-  	    		cfg_file = File.open('block.json','w')
-  	    		addSiteFile(cfg_file,$blockList_def,$blockList,@server.remote_addr)
-  	    	end
-  	    	cfg_file.close
-  	    end
-      end
 
     def unbind
       @server.close_connection_after_writing
@@ -135,9 +122,7 @@ module LocalServer
   attr_accessor :addr_to_send
   attr_accessor :server_using
   attr_accessor :cached_pieces
-  attr_accessor :isDirectConnect
-  attr_accessor :isCompleted
-  attr_accessor :dataCount
+  attr_accessor :isProxyConnect
 
   def post_init
     #puts "local connected"
@@ -150,17 +135,15 @@ module LocalServer
     @connector = nil
     @addr_to_send = ""
     @server_using = $server
-    @isDirectConnect = true
-    @isCompleted = false
-    @dataCount = 0
+    @isProxyConnect = true
+
   end
 
   def receive_data data
     if @stage == 5
-      if not @isDirectConnect
+      if @isProxyConnect
         encrypt $encrypt_table, data
       end
-      @dataCount += data.size
       @connector.send_data data
       return
     end
@@ -200,21 +183,21 @@ module LocalServer
         end
         #p @remote_addr, @remote_port
         #p @addr_to_send
-        @isDirectConnect = isDirectConnectFunc(@remote_addr)
-        if @isDirectConnect
-          puts "direct connecting #{@remote_addr} from localhost"
-        else
+        @isProxyConnect = isProxyConnectFunc(@remote_addr)
+        if @isProxyConnect
           puts "connecting #{@remote_addr} via #{$server}"
+        else          
+          puts "direct connecting #{@remote_addr} from localhost"
         end
         send_data "\x05\x00\x00\x01\x00\x00\x00\x00" + [@remote_port].pack('s>')
         @stage = 4
         if data.size > @header_length
           @cached_pieces.push data[@header_length, data.size]
         end
-        if @isDirectConnect
-          @connector = EventMachine.connect @remote_addr, @remote_port, LocalConnector, self
-        else
+        if @isProxyConnect
           @connector = EventMachine.connect $server, $remote_port, LocalConnector, self
+        else
+          @connector = EventMachine.connect @remote_addr, @remote_port, LocalConnector, self
         end
       rescue Exception => e
         warn e
@@ -230,23 +213,9 @@ module LocalServer
   end
 
   def unbind
-    if @isDirectConnect and (not @isCompleted)
-      puts "[WARNING] #{@remote_addr} direct connecting unbind,change to connecting by #{$server} proxy"
-      @isDirectConnect = false
-      reconnect $server, $remote_port
-      return
-    end
     if @connector != nil
       @connector.close_connection_after_writing
-      if not @isCompleted
-        puts "[ERROR] #{@remote_addr} remote connecting unbind,connection close"
-      end
-      puts "#{@remote_addr} connection close #{@dataCount}"
-      if (@dataCount < 2000) and (not isClassifyFunc(@remote_addr))
-        cfg_file = File.open('block.json','w')
-  	addSiteFile(cfg_file,$blockList_def,$blockList,@remote_addr)
-        cfg_file.close
-      end
+      add2OtherDictFile(@remote_addr, @remote_port)
     end
 
   end
